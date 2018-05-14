8ad4810e65d2a9b18ca306c3fa75f3cd2df88cfb
Christopher Maldonado <chmald@microsoft.com>
2018-05-14 18:34:56 -0500

    new changes

--- init_container.sh
+++ init_container.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-test ! -d "$APP_HOME" && echo "INFO: $APP_HOME not found. creating..." && mkdir -p "$APP_HOME" && npx create-react-app "$APP_HOME"
+test ! -d "$APP_HOME" && echo "INFO: $APP_HOME not found. creating..." && mkdir -p "$APP_HOME"
+
+if find "$1" -mindepth 1 -print -quit | grep -q .; then
+    echo "Application in directory"
+else
+    npx create-react-app "$APP_HOME"
+fi
 
 test ! -d "$HTTPD_LOG_DIR" && echo "INFO: $HTTPD_LOG_DIR not found. creating..." && mkdir -p "$HTTPD_LOG_DIR"
 

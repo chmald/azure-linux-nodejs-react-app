7572f65fba8ecf4d38b4d7230963a353a8f91928
Christopher Maldonado <christopher@maldonado.cloud>
2018-05-14 20:20:20 -0500

    Logging for app found

--- init_container.sh
+++ init_container.sh
@@ -5,6 +5,8 @@ test ! -d "$APP_HOME" && echo "INFO: $APP_HOME not found. creating..." && mkdir 
 if [ -z "$(ls -A $APP_HOME)" ]; then
     echo "Installing React in $APP_HOME..."
     npx create-react-app "$APP_HOME"
+else
+    echo "Application found..."
 fi
 
 test ! -d "$HTTPD_LOG_DIR" && echo "INFO: $HTTPD_LOG_DIR not found. creating..." && mkdir -p "$HTTPD_LOG_DIR"
